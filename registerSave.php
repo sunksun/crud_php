3d3a1dcb5c218e20a4e8775d653f1eb262273cc2
sunksun lapunt <sunksun.lap@MacBook-Pro-khxng-sunksun.local>
2023-11-17 11:45:26 +0700

    first commit

---
+++ registerSave.php
@@ -0,0 +1,22 @@
+<?php
+include_once('connect_db.php');
+
+$email = $_POST["email"];
+$pass_word = $_POST["pass_word"];
+$address1 = $_POST["address1"];
+$address2 = $_POST["address2"];
+$city = $_POST["city"];
+$state = $_POST["state"];
+$zip = $_POST["zip"];
+$check_me = $_POST["check_me"];
+
+$sql = "INSERT INTO `register` (`id`, `email`, `pass_word`, `address1`, `address2`, `city`, 
+`state`, `zip`, `check_me`, `date_time`) VALUES 
+(NULL, '$email', '$pass_word', '$address1', '$address2', '$city', '$state', '$zip', '$check_me', 
+current_timestamp());";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
