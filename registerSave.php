78e86187ceac2e84807a5d9e9f0317f988ffa6db
sunksun <sunksunlapunt@gmail.com>
2023-12-01 10:42:37 +0700

    01-12-2566

--- registerSave.php
+++ registerSave.php
@@ -10,15 +10,34 @@ $state = $_POST["state"];
 $zip = $_POST["zip"];
 $check_me = $_POST["check_me"];
 
-$sql = "INSERT INTO `register` (`id`, `email`, `pass_word`, `address1`, `address2`, `city`, 
+if (isset($_POST['save'])) {
+
+    $sql = "INSERT INTO `register` (`id`, `email`, `pass_word`, `address1`, `address2`, `city`, 
 `state`, `zip`, `check_me`, `date_time`) VALUES 
 (NULL, '$email', '$pass_word', '$address1', '$address2', '$city', '$state', '$zip', '$check_me', 
 current_timestamp());";
 
-if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($conn, $sql)) {
+        echo '<script language="JavaScript">';
+        echo 'alert("บันทึกข้อมูลเรียบร้อยแล้ว"); location.href = "register.php"';
+        echo '</script>';
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+} elseif (isset($_POST['update'])) {
+    $id = $_GET["id"];
+    $sql = "UPDATE `register` SET 
+    `email` = '$email', 
+    `pass_word` = '$pass_word', 
+    `address1` = '$address1', 
+    `address2` = '$address2', 
+    `city` = '$city', 
+    `state` = '$state', 
+    `zip` = '$zip', 
+    `check_me` = '$check_me' 
+    WHERE `register`.`id` = '$id';";
+    mysqli_query($conn, $sql);
     echo '<script language="JavaScript">';
-    echo 'alert("บันทึกข้อมูลเรียบร้อยแล้ว"); location.href = "register.php"';
+    echo 'alert("อัแเดทข้อมูลเรียบร้อยแล้ว"); location.href = "register.php"';
     echo '</script>';
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
